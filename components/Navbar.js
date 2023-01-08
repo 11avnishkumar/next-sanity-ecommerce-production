bd78896b77228f72eaa9c77408d1630486bfc199
Avnish <11avnishkumar@gmail.com>
2023-01-08 19:18:07 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -61,17 +61,17 @@ export default function Navbar() {
                         {item.name}
                       </a>
                     ))}
-                  </div>
-                  {/* Mobile search  */}
-                  <div className='hidden sm:block'>
-                    <form className='inline-flex items-center bg-gray-200 rounded-full px-3'>
-                      <input type="text" className="w-full bg-transparent border-0 outline-none py-2 px-3" placeholder="Search Products" />
-                      <button type="button" className="inline-flex w-6 h-6">
-                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                      </svg>
-                      </button>
-                    </form>
+                    {/* Mobile search  */}
+                    <div className='md:hidden'>
+                      <form className='inline-flex items-center bg-gray-200 rounded-full px-3'>
+                        <input type="text" className="w-full bg-transparent border-0 outline-none py-2 px-3" placeholder="Search Products" />
+                        <button type="button" className="inline-flex w-6 h-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                        </button>
+                      </form>
+                    </div>
                   </div>
                 </div>
               </div>
