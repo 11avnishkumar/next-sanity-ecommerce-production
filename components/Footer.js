9856087f95a3071a10ac19cd538046c9708a6005
Avnish <11avnishkumar@gmail.com>
2023-01-11 14:00:11 +0530

    updated

--- components/Footer.js
+++ components/Footer.js
@@ -3,22 +3,22 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='px-10 py-8 border-t border-gray-300'>
-      <div className='flex justify-between items-center'>
+      <div className='flex space-y-6 flex-col md:flex-row justify-between items-center'>
         <div>
-          <h1 className='text-4xl font-bold'>Subscribe to our newsletter</h1>
-          <p className='text-xl text-slate-600'>Stay up to date with the latest products.</p>
+          <h1 className='text-2xl md:text-4xl font-bold'>Subscribe to our newsletter</h1>
+          <p className='text-sm md:text-xl text-slate-600'>Stay up to date with the latest products.</p>
         </div>
         <div className='flex justify-between items-center gap-4'>
           <input type="text" className='border border-gray-300 rounded-md' placeholder='Enter your email'/>
           <input type="submit" value="Subscribe" className='bg-black text-white px-4 py-3 text-sm rounded-md' />
         </div>
       </div>
-      <div className='flex items-center border-b border-gray-300'>
-        <div className='w-2/3 px-6 py-6'>
+      <div className='flex flex-col md:flex-row items-center border-b border-gray-300'>
+        <div className='w-full md:w-2/3 px-6 py-6'>
           <p className='text-4xl text-emerald-400'><span className="text-9xl font-bold text-emerald-500">W</span>elcome, to the world&pos;s leading ecommerce plateform.</p>
         </div>
-        <div className='px-6 py-6'>
-          <div className='flex items-center justify-center gap-4'>
+        <div className='px-3 md:px-6 py-6'>
+          <div className='flex flex-col md:flex-row items-center justify-center gap-4'>
             <div>
               <p className='text-gray-400'>Company</p>
               <p>Lorem ipsum dolor sit amet.</p>
