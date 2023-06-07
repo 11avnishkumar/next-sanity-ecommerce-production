3c1d216d91ae39c6e88eef90aa2220ed5908dad5
avnish kumar <11avnishkumar@gmail.com>
2023-06-07 09:46:15 +0530

    fixed some ui

--- components/ProductReviewForm.js
+++ components/ProductReviewForm.js
@@ -1,27 +1,35 @@
 import React from 'react'
-import {useState} from 'react'
+import { useState } from 'react'
 /* import {client, urlFor} from '../../lib/client' */
-import {StarIcon} from '@heroicons/react/20/solid'
+import { StarIcon } from '@heroicons/react/20/solid'
 /* import {useStateContext} from '../../context/StateContext' */
 
 const ProductReviewForm = () => {
-    const [showProuductReviewForm,setShowProductReviewForm] = useState(false);
-    const [review,setReview] = useState(""); 
-    const handleProductReviews = (e) =>{
-     e.preventDefault();
-     console.log(review);
-    } 
-    return (
-        <div className='container mx-auto font-pop py-6'>
-            <div className='space-y-4 py-4 px-4 w-full md:grow'>
-                <form onSubmit={handleProductReviews}>
-                 <div className='form-group mb-3'>
-            <textarea className='resize-none border border-emerald-500 rounded-md' row="50" cols="50" placeholder='Write product reviews here' onChange={(e) => setReview(e.target.value)}></textarea>
-                 </div>
-                  <button type="submit" className='text-xl bg-emerald-500 text-white py-2 px-4 rounded-md focus:outline-emerald-300 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-emerald-700'>Submit Review</button>
-                </form>   
-             </div>
-        </div>
-    )
+  const [showProuductReviewForm, setShowProductReviewForm] = useState(false)
+  const [review, setReview] = useState('')
+  const handleProductReviews = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <div className='container mx-auto font-pop py-6'>
+      <div className='space-y-4 py-4 px-4 w-full md:grow'>
+        <form onSubmit={handleProductReviews}>
+          <div className='form-group mb-3'>
+            <textarea
+              className='resize-none border border-emerald-500 rounded-md w-full'
+              placeholder='Write product reviews here'
+              onChange={(e) => setReview(e.target.value)}
+            ></textarea>
+          </div>
+          <button
+            type='submit'
+            className='text-xl bg-emerald-500 text-white py-2 px-4 rounded-md focus:outline-emerald-300 focus:outline-none focus:ring focus:ring-offset-2 hover:bg-emerald-700'
+          >
+            Submit Review
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
-export default ProductReviewForm 
+export default ProductReviewForm
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,112 +1,136 @@
 import React from 'react'
-import {useState} from 'react'
-import {Product, ProductReviews} from '../../components'
-import {client, urlFor} from '../../lib/client'
-import {StarIcon} from '@heroicons/react/20/solid'
-import {useStateContext} from '../../context/StateContext'
+import { useState } from 'react'
+import { Product, ProductReviews } from '../../components'
+import { client, urlFor } from '../../lib/client'
+import { StarIcon } from '@heroicons/react/20/solid'
+import { useStateContext } from '../../context/StateContext'
 
-const ProductDetails = ({products, product}) => {
-    
-    const {image, name, price, details} = product
-    const [index, setIndex] = useState(0)
-    const {decQty, incQty, qty, onAdd} = useStateContext()
-    return (
-        <div className='container mx-auto font-pop py-6'>
-            <div className='flex flex-col md:flex-row gap-6'>
-            <div className='w-full md:w-2/3'>
-                <div className='bg-white shadow-md overflow-hidden rounded-md mb-4'>
-                    <img src={urlFor(image && image[index])} alt="w-full h-full rounded-lg"/>
+const ProductDetails = ({ products, product }) => {
+  const { image, name, price, details } = product
+  const [index, setIndex] = useState(0)
+  const { decQty, incQty, qty, onAdd } = useStateContext()
+  return (
+    <div className='container mx-auto font-pop py-6'>
+      <div className='flex flex-col md:flex-row gap-6'>
+        <div className='w-full md:w-2/3'>
+          <div className='bg-white shadow-md overflow-hidden rounded-md mb-4'>
+            <img
+              src={urlFor(image && image[index])}
+              alt='w-full h-full rounded-lg'
+            />
+          </div>
+          {/* Image gallary */}
+          <div className='image-gallary-container'>
+            <div className='mx-auto mt-3 grid grid-cols-4 bg-white'>
+              {image?.map((item, i) => (
+                <div key={i} className='overflow-hidden rounded-md'>
+                  <img
+                    src={urlFor(item)}
+                    alt=''
+                    className='h-full w-full'
+                    onMouseEnter={() => setIndex(i)}
+                  />
                 </div>
-                {/* Image gallary */}
-                <div className='image-gallary-container'>
-                    <div className='mx-auto mt-3 grid grid-cols-4 bg-white'>
-                        {image?.map((item, i) => (
-                            <div key={i} className="overflow-hidden rounded-md">
-                                <img src={urlFor(item)} alt="" className="h-full w-full"
-                                     onMouseEnter={() => setIndex(i)}/>
-                            </div>
-                        ))}
-                    </div>
-                </div>
+              ))}
             </div>
-            {/* right column */}
-            <div className='space-y-4 py-4 px-4 w-full md:grow'>
-                <p className='text-3xl text-slate-700 font-extrabold capitalize'>{name}</p>
-                <div className='flex justify-start items-center'>
-                    <StarIcon className='h-7 w-7 text-yellow-400'/>
-                    <StarIcon className='h-7 w-7 text-yellow-400'/>
-                    <StarIcon className='h-7 w-7 text-yellow-400'/>
-                    <StarIcon className='h-7 w-7 text-yellow-400'/>
-                    ({20})
-                </div>
-                <hr/>
-                <div className='mt-4'>
-                    <p className='text-3xl font-extrabold mb-2'>$ {price}</p>
-                    <p className='text-sm mb-2'>{details}</p>
-                    <p className='text-emerald-500 text-2xl'>In Stock</p>
-                </div>
-                <hr/>
-                <div className='flex gap-4 justify-center items-center bg-gray-200 rounded-full md:w-1/2'>
-                    <span className='px-3 py-4 cursor-pointer text-2xl text-center block  rounded-md'
-                          onClick={decQty}>-</span> <span className='text-2xl'>{qty}</span>
-                    <span className='px-3 py-4 cursor-pointer text-2xl text-center block  rounded-md'
-                          onClick={incQty}>+</span>
-                </div>
-                <div className="flex items-center gap-2 mx-auto">
-                    <input type="button" value="Add to cart"
-                           className="border border-2 border-emerald-500 text-emerald-500 py-4 w-full cursor-pointer rounded-full text-xl"
-                           onClick={() => onAdd(product, qty)}/>
-                    <input type="button" value="Buy Now"
-                           className='bg-emerald-500 py-4 w-full cursor-pointer rounded-full text-white text-xl'/>
-                </div>
-            </div>
+          </div>
         </div>
-       {/* Product Reviews */}
-        <ProductReviews/>
-        {/* Product Recommendation  */}
-        <div className='mt-4 px-4'>
-            <h1 className='text-3xl text-slate-700 font-extrabold capitalize md:text-3xl'>You may also like</h1>
-            <hr/>
-            <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
-            {products.map((item) => (
-                <Product key={item._id} product={item}/>
-            ))}
-            </div>
+        {/* right column */}
+        <div className='space-y-4 py-4 px-4 w-full md:grow'>
+          <p className='text-3xl text-slate-700 font-extrabold capitalize'>
+            {name}
+          </p>
+          <div className='flex justify-start items-center'>
+            <StarIcon className='h-7 w-7 text-yellow-400' />
+            <StarIcon className='h-7 w-7 text-yellow-400' />
+            <StarIcon className='h-7 w-7 text-yellow-400' />
+            <StarIcon className='h-7 w-7 text-yellow-400' />({20})
+          </div>
+          <hr />
+          <div className='mt-4'>
+            <p className='text-3xl font-extrabold mb-2'>$ {price}</p>
+            <p className='text-sm mb-2'>{details}</p>
+            <p className='text-emerald-500 text-2xl'>In Stock</p>
+          </div>
+          <hr />
+          <div className='flex gap-4 justify-center items-center bg-gray-200 rounded-full md:w-1/2'>
+            <span
+              className='px-3 py-4 cursor-pointer text-2xl text-center block  rounded-md'
+              onClick={decQty}
+            >
+              -
+            </span>{' '}
+            <span className='text-2xl'>{qty}</span>
+            <span
+              className='px-3 py-4 cursor-pointer text-2xl text-center block  rounded-md'
+              onClick={incQty}
+            >
+              +
+            </span>
+          </div>
+          <div className='flex items-center gap-2 mx-auto'>
+            <input
+              type='button'
+              value='Add to cart'
+              className='border border-2 border-emerald-500 text-emerald-500 py-4 w-full cursor-pointer rounded-full text-xl'
+              onClick={() => onAdd(product, qty)}
+            />
+            <input
+              type='button'
+              value='Buy Now'
+              className='bg-emerald-500 py-4 w-full cursor-pointer rounded-full text-white text-xl'
+            />
+          </div>
         </div>
+      </div>
+      {/* Product Reviews */}
+      <ProductReviews />
+      {/* Product Recommendation  */}
+      <div className='space-y-4 mt-4 px-4'>
+        <h1 className='text-3xl text-slate-700 font-extrabold capitalize'>
+         You may also like 
+        </h1>
+        <hr />
+        <div className='grid grid-cols-1 md:grid-cols-4 gap-4'>
+          {products.map((item) => (
+            <Product key={item._id} product={item} />
+          ))}
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default ProductDetails
 
 export const getStaticPaths = async () => {
-    const query = `*[_type == "product"] {
+  const query = `*[_type == "product"] {
     slug {
       current
     }
   }`
-    const products = await client.fetch(query)
+  const products = await client.fetch(query)
 
-    const paths = products.map((product) => ({
-        params: {
-            slug: product.slug.current
-        }
-    }));
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }))
 
-    return {
-        paths,
-        fallback: 'blocking'
-    }
+  return {
+    paths,
+    fallback: 'blocking',
+  }
 }
 
-export const getStaticProps = async ({params: {slug}}) => {
-    const query = `*[_type == "product" && slug.current == '${slug}'][0]`
-    const productQuery = '*[_type== "product"]'
-    
-    /* Product for You may like section*/
-    const product = await client.fetch(query);
-    const products = await client.fetch(productQuery);
-    return {
-        props: {products, product}
-    }
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`
+  const productQuery = '*[_type== "product"]'
+
+  /* Product for You may like section*/
+  const product = await client.fetch(query)
+  const products = await client.fetch(productQuery)
+  return {
+    props: { products, product },
+  }
 }
